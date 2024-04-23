3e3103fbfefaf79d46e8d7e73459160140c794dc
Laurel Howell <lhowel01@syr.edu>
2024-04-22 21:46:42 -0400

    sentiment analysis commit

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -5,7 +5,7 @@ from textblob import TextBlob
 df = pd.read_csv('data/bigboy.csv')
 
 # Define the keywords you want to analyze for sentiment
-keywords = ['health', 'wellness', '12-3-30']
+keywords = ['mental health'] #deleting and adding keywords as I run the program to save time
 
 # Function to calculate sentiment polarity using TextBlob
 def calculate_sentiment(text):
@@ -18,20 +18,22 @@ positive_sentiment_counts = {keyword: 0 for keyword in keywords}
 # Iterate over each keyword
 for keyword in keywords:
     # Filter rows containing the current keyword
-    filtered_df = df[df['full_text'].str.contains(keyword, case=False)]
+    filtered_df = df[df['full_text'].str.contains(keyword, case=False)].copy()
     
     # Apply sentiment analysis to filtered rows
     filtered_df.loc[:, 'sentiment_score'] = filtered_df['full_text'].apply(calculate_sentiment)
+
+    total_tweets = len(filtered_df)
     
     # Count the number of positive sentiment occurrences
     positive_count = (filtered_df['sentiment_score'] > 0).sum()
     
     # Update the count in the dictionary
-    positive_sentiment_counts[keyword] = positive_count
+    positive_sentiment_counts[keyword] = [positive_count, total_tweets]
     
     # Print the keyword and the count of positive sentiment occurrences
     # print(f"Keyword: {keyword}, Positive Sentiment Count: {positive_count}")
 
 # Print the dictionary containing positive sentiment counts for each keyword
-print("Positive Sentiment Counts for Each Keyword:")
+print("Positive Sentiment Counts Compared to Total Count for Each Keyword:")
 print(positive_sentiment_counts)
